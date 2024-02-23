88c0f5e32c0c6b6776da6a4ee2cfdb13c4850f1c
Julie Cao <juliehongcao@gmail.com>
2024-02-23 15:22:13 -0800

    feat: render account statistics info

--- HealthMate/screens/AccountScreen.js
+++ HealthMate/screens/AccountScreen.js
@@ -6,7 +6,18 @@ import { useContext } from "react";
 import { AuthContext } from "../store/auth-context";
 import BubbleWithCharacter from "../components/ui/BubbleWithCharacter";
 
+
+const Value = ({ label, value }) => {
+  return (
+    <View style={styles.infoContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.value}>{value}</Text>
+    </View>
+  );
+};
+
 const AccountScreen = () => {
+  
   authCtx = useContext(AuthContext);
   return (
     <View style={styles.rootContainer}>
@@ -20,6 +31,21 @@ const AccountScreen = () => {
           </Text>
         </View>
       </BubbleWithCharacter>
+      <View style={styles.infoContainer}>
+        <View>
+          <Value label="Height" value="7' 0" />
+          <Value label="Weight" value="165.2 lbs" />
+          <Value label="Body fat %" value="35.3%" />
+          <Value label="Body mass index" value="25" />
+          <Value label="Active Energy" value="102.5 cal" />
+          <Value label="Resting Energy" value="52.6 cal" />
+          <Value label="Step count (week avg)" value="2,345 steps" />
+          <Value label="Time in bed (week avg)" value="7h 32m" />
+
+        </View>
+        
+        
+      </View>
       <View style={{ flex: 1, justifyContent: "flex-end", marginBottom: 90 }}>
         <View
           style={{
@@ -54,5 +80,14 @@ const styles = StyleSheet.create({
 
   button: {
     marginHorizontal: 0,
+  },
+  label: {
+    fontSize: 14,
+    color: 'white',
+  },
+  value: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: Colors.primary500,
   },
 });
