5fe44aa5751311e41b208ca2b6716ac5739318b4
AprilFoolsDay0401 <dkkim6@uci.edu>
2024-03-15 17:34:06 -0700

    Merge with other branch

--- HealthMate/screens/MealsOverviewScreen.js
+++ HealthMate/screens/MealsOverviewScreen.js
@@ -1,21 +1,17 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  Dimensions,
-  Pressable,
-  Alert,
-} from "react-native";
+import { View, Text, StyleSheet, Dimensions, Pressable } from "react-native";
+import { useState } from "react";
 import { Colors } from "../constants/styles";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Button from "../components/ui/Button";
+import OptionalPopup from "../components/ui/OptionalPopup";
 
 const deviceWidth = Dimensions.get("window").width;
 
 const MealsOverviewScreen = ({ route }) => {
   const { title, data } = route.params;
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   // console.log("MealsOverviewScreen", data);
 
@@ -23,45 +19,44 @@ const MealsOverviewScreen = ({ route }) => {
 
   let totalCalories = 0;
 
-  const updateTotalCalories = () => {
-    Alert.alert(
-      "Need to confirm!",
-      `Its calories is ${totalCalories} cal\n Are you sure to eat this food?!`,
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel",
-        },
-        {
-          text: "OK",
-          onPress: async () => {
-            // console.log("Take it!!!: ", totalCalories);
-            const now = new Date();
-            const offsetInHours = -14;
-            const currentDate = new Date(
-              now.getTime() +
-                (offsetInHours * 60 + now.getTimezoneOffset()) * 60000
-            );
-            // console.log(currentDate);
-            try {
-              const existingData = await AsyncStorage.getItem(
-                "caloriesIntakeData"
-              );
-              let newData = JSON.parse(existingData) || [];
-              newData.push({ date: currentDate, value: totalCalories });
-              await AsyncStorage.setItem(
-                "caloriesIntakeData",
-                JSON.stringify(newData)
-              );
-              // console.log("Calories data saved successfully");
-            } catch (error) {
-              console.log("Failed to save calories data", error);
-            }
-          },
-        },
-      ]
+  const handleGobblePress = () => {
+    setIsPopupVisible(true);
+  };
+
+  const updateTotalCalories = async () => {
+    // console.log("Take it!!!: ", totalCalories);
+    const now = new Date();
+    const offsetInHours = -14;
+    const currentDate = new Date(
+      now.getTime() + (offsetInHours * 60 + now.getTimezoneOffset()) * 60000
     );
+    console.log(currentDate);
+    try {
+      const existingData = await AsyncStorage.getItem("caloriesIntakeData");
+      let newData = JSON.parse(existingData) || [
+        { date: "2024-03-01T00:00:00.000Z", value: 2499 },
+        { date: "2024-03-02T00:00:00.000Z", value: 2803 },
+        { date: "2024-03-03T00:00:00.000Z", value: 2974 },
+        { date: "2024-03-04T00:00:00.000Z", value: 2863 },
+        { date: "2024-03-05T00:00:00.000Z", value: 2924 },
+        { date: "2024-03-06T00:00:00.000Z", value: 2175 },
+        { date: "2024-03-07T00:00:00.000Z", value: 2936 },
+        { date: "2024-03-08T00:00:00.000Z", value: 2715 },
+        { date: "2024-03-09T00:00:00.000Z", value: 2333 },
+        { date: "2024-03-10T00:00:00.000Z", value: 2998 },
+        { date: "2024-03-11T00:00:00.000Z", value: 2818 },
+        { date: "2024-03-12T00:00:00.000Z", value: 2036 },
+        { date: "2024-03-13T00:00:00.000Z", value: 2675 },
+        { date: "2024-03-14T15:14:47.291Z", value: 2140 },
+        { date: "2024-03-15T00:43:55.561Z", value: 340 },
+      ];
+      console.log(newData);
+      newData.push({ date: currentDate, value: totalCalories });
+      await AsyncStorage.setItem("caloriesIntakeData", JSON.stringify(newData));
+      // console.log("Calories data saved successfully");
+    } catch (error) {
+      console.log("Failed to save calories data", error);
+    }
   };
 
   const switchToMealOverview = (name, mData) => {
@@ -95,8 +90,15 @@ const MealsOverviewScreen = ({ route }) => {
           Total Calories:{"     "} {totalCalories} cal
         </Text>
         <View style={styles.button}>
-          <Button onPress={updateTotalCalories}>Gobble</Button>
+          <Button onPress={handleGobblePress}>Gobble</Button>
         </View>
+        <OptionalPopup
+          isVisible={isPopupVisible}
+          onClose={() => setIsPopupVisible(!isPopupVisible)}
+          title="Food Confirmation"
+          message={`This meal is ${totalCalories} cal.\n Do you still want to eat this food?`}
+          onPressYes={updateTotalCalories}
+        />
       </View>
     </View>
   );
